fae21f9146c22ed090356cf3cf76b7c6a8d2fa7d
Ali Hamidi <allii09300hamidi@gmail.com>
2025-03-26 02:47:57 +0430

    create add , delete , toggle actions

--- src/redux/todoslice.ts
+++ src/redux/todoslice.ts
@@ -1,9 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+const initialState: Todo[] = [];
 
 const todoSlice = createSlice({
   name: "todos",
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      const newTodo: Todo = {
+        id: Date.now(),
+        text: action.payload,
+        completed: false,
+      };
+      return [...state, newTodo];
+    },
+    deleteTodo: (state, action: PayloadAction<number>) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+    toggleTodo: (state, action: PayloadAction<number>) => {
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
+      );
+    },
+  },
 });
 
 export default todoSlice.reducer;
+export const { addTodo, deleteTodo, toggleTodo } = todoSlice.actions;
